dfaac3126ce4fb9daac5bb68cbfffca88a01d12a
ayaz <ayazahmed0441@gmail..com>
2020-03-14 00:40:46 +0500

    Upload first time

---
+++ app.js
@@ -0,0 +1,82 @@
+//Require Work Functions
+// const function1 = require('./functions');
+// console.log(function1.sum(23,23));
+
+//Working on event
+
+const EventEmitter = require('events');
+// const EventEmitter = new event.EventEmitter;
+
+// EventEmitter.on('notification',(...arg)=>{
+
+//     console.log(arg[0]+arg[1]);
+
+// });
+// const eventdata = EventEmitter.emit('notification',1,2);
+
+
+//Class With Emittt Event
+// class person extends EventEmitter{
+//     constructor(name){
+//         super();
+//         this._name = name;
+//     }
+//     get name(){
+//        return this._name;
+//     }
+// }
+// let pr = new person('Ayaz');
+// let email = new person('ayazahmed0441@gmail.com');
+
+// email.on('email',()=>{
+//    console.log('This is email:'+email.name); 
+// })
+// email.emit('email');
+
+// pr.on('name',() =>{
+//     console.log('This is '+pr.name);
+// })
+// pr.emit('name');
+
+//Start Work On Readline
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input : process.stdin,
+//     output : process.stdout,
+// });
+// let num1 = Math.floor((Math.random() * 10) +1 );
+// let num2 = Math.floor((Math.random() * 10) +1 );
+// let answer = num1+num2;
+// rl.question(`What is the ${num1} + ${num2} answer is :`,(userinput) =>{
+//     if(userinput.trim() == answer){
+//             rl.close();
+//     }else{
+//       rl.setPrompt('The is incorrect Please Try Again \n');
+//       rl.prompt();
+//       rl.on('line',(userinput) => {
+//         if(userinput.trim() == answer){
+//             rl.close();
+//         }else{
+//           rl.setPrompt(`Answer Is in ${userinput} Please Try Agin \n`);
+//           rl.prompt();
+//         }
+//       });
+//     }
+// }
+// );
+// rl.on('close',() => {
+//     console.log('Correct !!!!!');
+// });
+
+//File Working File
+
+const fs = require('fs');
+fs.writeFile('example.txt','This is just for testing',(arr) =>{
+
+    if(arr)
+console.log(arr);
+    else
+        console.log('File Created Successfuly!!!');
+    
+
+});
---
+++ functions.js
@@ -0,0 +1,7 @@
+const sum = (num1,num2) => num1 + num2;
+const PI = 563.1;
+
+
+
+
+//module.exports = {sum:sum , PI:PI};
